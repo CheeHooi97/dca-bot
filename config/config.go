293d9fd0213f6b8cab424834a593b2cc2c3dc074
CheeHooi97 <57720168+CheeHooi97@users.noreply.github.com>
2025-12-04 22:04:43 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -10,28 +10,13 @@ import (
 var (
 	BinanceApiKey    string
 	BinanceApiSecret string
-	// BTC4h            string
-	// BTC1d            string
-	// ETH4h            string
-	// ETH1d            string
-	// ADA4h            string
-	// ADA1d            string
-	// SOL4h            string
-	// SOL1d            string
-	// BNB4h            string
-	// XRP4h            string
-	// DOGE4h           string
-	// SUI4h            string
-	// LINK4h           string
-	// AVAX4h           string
-	// TON4h            string
-	// DOT4h            string
-	// THETA4h          string
-	// WLD4h            string
-	// TIA4h            string
-	// TRUMP4h          string
+	BTC2             string
+	BTC1             string
+	BTC1x5           string
+	ETH2             string
+	ETH1             string
+	ETH1x5           string
 	TelegramChatId   string
-	DCAToken         string
 )
 
 // LoadConfig
@@ -40,28 +25,13 @@ func LoadConfig() {
 
 	BinanceApiKey = GetEnv("BINANCE_API_KEY")
 	BinanceApiSecret = GetEnv("BINANCE_API_SECRET")
-	// BTC4h = GetEnv("BTC_4h")
-	// BTC1d = GetEnv("BTC_1d")
-	// ETH4h = GetEnv("ETH_4h")
-	// ETH1d = GetEnv("ETH_1d")
-	// ADA4h = GetEnv("ADA_4h")
-	// ADA1d = GetEnv("ADA_1d")
-	// SOL4h = GetEnv("SOL_4h")
-	// SOL1d = GetEnv("SOL_1d")
-	// BNB4h = GetEnv("BNB_4h")
-	// XRP4h = GetEnv("XRP_4h")
-	// DOGE4h = GetEnv("DOGE_4h")
-	// SUI4h = GetEnv("SUI_4h")
-	// LINK4h = GetEnv("LINK_4h")
-	// AVAX4h = GetEnv("AVAX_4h")
-	// TON4h = GetEnv("TON_4h")
-	// DOT4h = GetEnv("DOT_4h")
-	// THETA4h = GetEnv("THETA_4h")
-	// WLD4h = GetEnv("WLD_4h")
-	// TIA4h = GetEnv("TIA_4h")
-	// TRUMP4h = GetEnv("TRUMP_4h")
+	BTC2 = GetEnv("BTC2")
+	BTC1 = GetEnv("BTC1")
+	BTC1x5 = GetEnv("BTC1x5")
+	ETH2 = GetEnv("ETH2")
+	ETH1 = GetEnv("ETH1")
+	ETH1x5 = GetEnv("ETH1x5")
 	TelegramChatId = GetEnv("TELEGRAM_CHAT_ID")
-	DCAToken = GetEnv("DCA_TOKEN")
 }
 
 func GetEnv(key string) string {
