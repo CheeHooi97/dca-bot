3eee04ee9545f3fc8c6499cba60f70455ff0bcb2
CheeHooi97 <57720168+CheeHooi97@users.noreply.github.com>
2025-12-04 21:46:11 +0800

    Create symbol.go

---
+++ constant/symbol.go
@@ -0,0 +1,18 @@
+package constant
+
+import "dca-bot/config"
+
+func GetTokenMap() map[string]any {
+	return map[string]any{
+		"btcusdt": map[float64]string{
+			2:   config.BTC2,
+			1.5: config.BTC1x5,
+			1:   config.BTC1,
+		},
+		"ethusdt": map[float64]string{
+			2:   config.ETH2,
+			1.5: config.ETH1x5,
+			1:   config.ETH1,
+		},
+	}
+}
