02092cdacca8d0ba884debe3e33ca18ccd59937d
CheeHooi97 <57720168+CheeHooi97@users.noreply.github.com>
2025-12-17 17:38:17 +0800

    Update main_copy.go

--- main_copy.go
+++ main_copy.go
@@ -1,65 +1,75 @@
 package main
 
-// func main() {
-// 	// Load config
-// 	config.LoadConfig()
+import (
+	"bufio"
+	"dca-bot/config"
+	"dca-bot/service"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-// 	reader := bufio.NewReader(os.Stdin)
+func main() {
+	// Load config
+	config.LoadConfig()
 
-// 	// --- Input: Symbol ---
-// 	fmt.Print("Enter trading pair (e.g. btcusdt): ")
-// 	symbolInput, _ := reader.ReadString('\n')
-// 	symbol := strings.TrimSpace(symbolInput)
+	reader := bufio.NewReader(os.Stdin)
 
-// 	// --- Input: Total USDT for DCA ---
-// 	fmt.Print("Enter total USDT budget for DCA: ")
-// 	usdtInput, _ := reader.ReadString('\n')
-// 	usdtStr := strings.TrimSpace(usdtInput)
+	// --- Input: Symbol ---
+	fmt.Print("Enter trading pair (e.g. btcusdt): ")
+	symbolInput, _ := reader.ReadString('\n')
+	symbol := strings.TrimSpace(symbolInput)
 
-// 	totalUSDT, err := strconv.ParseFloat(usdtStr, 64)
-// 	if err != nil {
-// 		fmt.Println("Invalid USDT amount")
-// 		return
-// 	}
+	// --- Input: Total USDT for DCA ---
+	fmt.Print("Enter total USDT budget for DCA: ")
+	usdtInput, _ := reader.ReadString('\n')
+	usdtStr := strings.TrimSpace(usdtInput)
 
-// 	// --- Input: Drop percent trigger ---
-// 	fmt.Print("Enter drop percentage trigger (e.g. 1.5): ")
-// 	dropInput, _ := reader.ReadString('\n')
-// 	dropStr := strings.TrimSpace(dropInput)
+	totalUSDT, err := strconv.ParseFloat(usdtStr, 64)
+	if err != nil {
+		fmt.Println("Invalid USDT amount")
+		return
+	}
 
-// 	dropPercent, err := strconv.ParseFloat(dropStr, 64)
-// 	if err != nil {
-// 		fmt.Println("Invalid drop percent")
-// 		return
-// 	}
+	// --- Input: Drop percent trigger ---
+	fmt.Print("Enter drop percentage trigger (e.g. 1.5): ")
+	dropInput, _ := reader.ReadString('\n')
+	dropStr := strings.TrimSpace(dropInput)
 
-// 	fmt.Print("Fallback Buy Again: ")
-// 	fallbackBuyHoursInput, _ := reader.ReadString('\n')
-// 	fallbackBuyHoursStr := strings.TrimSpace(fallbackBuyHoursInput)
+	dropPercent, err := strconv.ParseFloat(dropStr, 64)
+	if err != nil {
+		fmt.Println("Invalid drop percent")
+		return
+	}
 
-// 	fallbackBuyHours, _ := strconv.ParseInt(fallbackBuyHoursStr, 10, 64)
+	fmt.Print("Fallback Buy Again: ")
+	fallbackBuyHoursInput, _ := reader.ReadString('\n')
+	fallbackBuyHoursStr := strings.TrimSpace(fallbackBuyHoursInput)
 
-// 	fmt.Print("Enter sell percentage(e.g. 1.5): ")
-// 	sellInput, _ := reader.ReadString('\n')
-// 	sellStr := strings.TrimSpace(sellInput)
+	fallbackBuyHours, _ := strconv.ParseInt(fallbackBuyHoursStr, 10, 64)
 
-// 	sellPercent, err := strconv.ParseFloat(sellStr, 64)
-// 	if err != nil {
-// 		fmt.Println("Invalid drop percent")
-// 		return
-// 	}
+	fmt.Print("Enter sell percentage(e.g. 1.5): ")
+	sellInput, _ := reader.ReadString('\n')
+	sellStr := strings.TrimSpace(sellInput)
 
-// 	// Initialize service
-// 	dcaService := service.NewDCAService()
+	sellPercent, err := strconv.ParseFloat(sellStr, 64)
+	if err != nil {
+		fmt.Println("Invalid drop percent")
+		return
+	}
 
-// 	// Start DCA bot
-// 	err = dcaService.Start(symbol, totalUSDT, dropPercent, sellPercent, int(fallbackBuyHours))
-// 	if err != nil {
-// 		fmt.Println("Error starting DCA:", err)
-// 		return
-// 	}
+	// Initialize service
+	dcaService := service.NewDCAService()
 
-// 	// Keep the program alive so goroutine runs
-// 	fmt.Println("DCA bot started... (CTRL+C to exit)")
-// 	select {}
-// }
+	// Start DCA bot
+	err = dcaService.Start(symbol, totalUSDT, dropPercent, sellPercent, int(fallbackBuyHours))
+	if err != nil {
+		fmt.Println("Error starting DCA:", err)
+		return
+	}
+
+	// Keep the program alive so goroutine runs
+	fmt.Println("DCA bot started... (CTRL+C to exit)")
+	select {}
+}
